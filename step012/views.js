f4f7f8c9c02b897144b0332716e4e71399132454
Zev Spitz <shivisi@hotmail.com>
2022-08-09 23:01:21 +0300

    Generated table

--- step012/views.js
+++ step012/views.js
@@ -20,6 +20,26 @@ let lastContainer = document.querySelector('#single-image-container');
         if (lastContainer) { lastContainer.classList.add('d-none');}
         container.classList.remove('d-none');
 
+        switch (key) {
+            case 'slide':
+                renderPicture();
+                break;
+            case 'list':
+                renderTable();
+                break;
+        }
+
         lastContainer = container;
     });
 });
+
+const renderTable = () => {
+    document.querySelector('#list-container tbody').innerHTML = pictureData.map(x => {
+        return `<tr>
+    <td><img style="height: 30px; width:auto;" src="images/${x.name}"></td>
+    <td>${x.title}</td>
+    <td>${x.credits}</td>
+    <td>${x.featured}</td>
+</tr>`;
+    }).join('');
+};
