8f2ada7d6bddf97fce6df31e6713d4f773f00f18
bk-software <barak.software.solutions@gmail.com>
2022-08-10 18:12:09 +0300

    Bugfix - pressing two time cause page to disappear

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ let currentPageId = 'home-page';
 
 function makeMenuLinkChangePage(linkId, pageToShow) {
   const pageLinkElement = document.getElementById(linkId);
+  console.log(pageLinkElement);
 
   pageLinkElement.addEventListener('click', function () {
     console.log('you clicked me');
+    if (currentPageId == pageToShow) {
+      return;
+    }
+
+    console.log(' after the if');
+
+    // Show the page
     document.getElementById(pageToShow).classList.remove('d-none');
 
+    // Hide the page
     document.getElementById(currentPageId).classList.add('d-none');
 
     currentPageId = pageToShow;
