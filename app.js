803c7378a6bc968d141f17e852a425533130aa2a
bk-software <barak.software.solutions@gmail.com>
2022-08-10 18:24:49 +0300

    Change the function to be not anyonmouse function

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 let currentPageId = 'home-page';
 
+function showPage(pageIdToShow) {
+  console.log('you clicked me');
+  if (currentPageId == pageIdToShow) {
+    return;
+  }
+
+  console.log(' after the if');
+
+  // Show the page
+  document.getElementById(pageIdToShow).classList.remove('d-none');
+
+  // Hide the page
+  document.getElementById(currentPageId).classList.add('d-none');
+
+  currentPageId = pageIdToShow;
+}
+
 function makeMenuLinkChangePage(linkId, pageToShow) {
   const pageLinkElement = document.getElementById(linkId);
-  console.log(pageLinkElement);
-
   pageLinkElement.addEventListener('click', function () {
-    console.log('you clicked me');
-    if (currentPageId == pageToShow) {
-      return;
-    }
-
-    console.log(' after the if');
-
-    // Show the page
-    document.getElementById(pageToShow).classList.remove('d-none');
-
-    // Hide the page
-    document.getElementById(currentPageId).classList.add('d-none');
-
-    currentPageId = pageToShow;
+    showPage(pageToShow);
   });
 }
 
