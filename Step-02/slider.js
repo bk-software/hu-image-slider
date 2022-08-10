fcd27ea505a450fdc4e4349b612176e3a9c3aace
bk-software <barak.software.solutions@gmail.com>
2022-08-10 20:55:37 +0300

    button switch images

--- Step-02/slider.js
+++ Step-02/slider.js
@@ -1,9 +1,12 @@
 function prevImage() {
   console.log('You press prev image');
+  document.getElementById('slider-image').src =
+    'images/OHR.LongsPeak_ROW9098186381_1920x1080.jpg';
 }
 
 function nextImage() {
-  console.log('You press prev image');
+  document.getElementById('slider-image').src =
+    'images/OHR.NoctilucentClouds_ROW9144099035_1920x1080.jpg';
 }
 
 document.getElementById('prev').addEventListener('click', prevImage);
