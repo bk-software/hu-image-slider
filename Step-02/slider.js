d3a4643ce9df0d1755f16bc2ebd1edde3809936b
bk-software <barak.software.solutions@gmail.com>
2022-08-10 21:12:23 +0300

    take image from an array

--- Step-02/slider.js
+++ Step-02/slider.js
@@ -1,12 +1,19 @@
+const imagesArray = [
+  'OHR.HickmanBridge_ROW9566895207_1920x1080.jpg',
+  'OHR.LongsPeak_ROW9098186381_1920x1080.jpg',
+  'OHR.NoctilucentClouds_ROW9144099035_1920x1080.jpg',
+  'OHR.SpiralHill_ROW7328923046_1920x1080.jpg',
+];
+
 function prevImage() {
   console.log('You press prev image');
-  document.getElementById('slider-image').src =
-    'images/OHR.LongsPeak_ROW9098186381_1920x1080.jpg';
+  const imageURL = 'images/' + imagesArray[0];
+  document.getElementById('slider-image').src = imageURL;
 }
 
 function nextImage() {
-  document.getElementById('slider-image').src =
-    'images/OHR.NoctilucentClouds_ROW9144099035_1920x1080.jpg';
+  const imageURL = 'images/' + imagesArray[1];
+  document.getElementById('slider-image').src = imageURL;
 }
 
 document.getElementById('prev').addEventListener('click', prevImage);
