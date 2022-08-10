8035239e6fe8a95025cb4567c9dd6707e6b96947
bk-software <barak.software.solutions@gmail.com>
2022-08-10 20:20:20 +0300

    add event listeners to the image slider buttons

--- Step-02/slider.js
+++ Step-02/slider.js
@@ -1,1 +1,10 @@
-console.log('slider js');
+function prevImage() {
+  console.log('You press prev image');
+}
+
+function nextImage() {
+  console.log('You press prev image');
+}
+
+document.getElementById('prev').addEventListener('click', prevImage);
+document.getElementById('next').addEventListener('click', nextImage);
